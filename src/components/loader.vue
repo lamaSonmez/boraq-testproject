5cc5089640ab0a02c8ab19d56298f1cebd5f9e59
lama <lamasonmez@gmail.com>
2019-09-01 10:21:36 +0300

    add loader component

---
+++ src/components/loader.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="grid-loader">
+        <vue-loaders-line-scale-party color="#3EAAFD"></vue-loaders-line-scale-party>
+    </div>
+</template>
+<script>
+export default {
+    name:'Loader',
+}  
+</script>
+<style lang="scss">
+@import '../../node_modules/vue-loaders/dist/vue-loaders.css';
+
+    .grid-loader{
+      text-align:center;
+        width:100%;
+        padding:20px 0px;
+        min-height:100px;
+    }
+</style>
--- src/main.js
+++ src/main.js
@@ -3,13 +3,17 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueLoaders from 'vue-loaders';
+
+Vue.use(VueLoaders);
+
 
 Vue.config.productionTip = false
-
+window.apiHost = "https://5d692f278134fd001430c45a.mockapi.io/api";
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
+    el: '#app',
+    router,
+    components: { App },
+    template: '<App/>'
 })
